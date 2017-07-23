b461ae8fa902e51672c4c6ab6c8ce5510ac35c98
santosh <santosh.yadav198613@gmail.com>
2017-07-23 14:26:24 +0530

    added bindings and onInit

--- app/component/post.component.js
+++ app/component/post.component.js
@@ -3,11 +3,15 @@ angular.module('myApp').component('postComponent', {
     controller: function () {
         this.title = "This is my first component";
         this.post;
+        this.name;
+
+        this.$onInit = function () {
+            this.name = 'hello from init method';
+        };
     },
     bindings: {
         post: '=',
-        title : '@',
-
+        title: '@'
     }
 });
 
